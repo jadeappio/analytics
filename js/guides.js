c3cf46a36f37f29c7a922d8d6b9a47a9672c0639
jadeappio <83730972+jadeappio@users.noreply.github.com>
2021-08-17 16:53:31 -0400

    fix syntax error guides.js

--- js/guides.js
+++ js/guides.js
@@ -4,4 +4,4 @@ var script= document.createElement('script');
 script.type= 'text/javascript';
 script.src= 'https://player.kompassify.com/kompassifyPlayer.js?uuid=782283f5-ce0f-4d29-a02b-65eb5196365b';
 script.id='kompassious';
-body.appendChild('script');
+body.appendChild(script);
