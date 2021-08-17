18d74814072314f110a299fe89fcd246954eb73f
jadeappio <83730972+jadeappio@users.noreply.github.com>
2021-08-17 16:33:55 -0400

    update guides.js

---
+++ js/guides.js
@@ -0,0 +1,7 @@
+console.log('jade guides loaded');
+var body= document.getElementsByTagName('body')[0];
+var script= document.createElement('script');
+script.type= 'text/javascript';
+script.src= 'https://player.kompassify.com/kompassifyPlayer.js?uuid=782283f5-ce0f-4d29-a02b-65eb5196365b';
+script.id='kompassious';
+body.appendChild(script);
